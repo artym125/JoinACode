37e2d4b486084085e1f5e4be69cb03a05ffae2ab
OstapArtym <artym.ostap@icloud.com>
2024-01-26 11:44:32 +0200

    Initial Commit

---
+++ JoinACode/JoinACodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoinACodeApp.swift
+//  JoinACode
+//
+//  Created by Ostap Artym on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct JoinACodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
